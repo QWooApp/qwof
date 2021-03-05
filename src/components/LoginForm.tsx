f86183fda122921383faa04fc6d78fd641efa1b2
mentix02 <manan.yadav02@gmail.com>
2021-03-05 14:57:20 +0530

    Added visibility icon to password field.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, MouseEvent } from "react";
 import { useHistory, Link as RouterLink } from "react-router-dom";
 
 import Box from "@material-ui/core/Box";
@@ -8,6 +8,14 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import InputLabel from "@material-ui/core/InputLabel";
+import Visibility from "@material-ui/icons/Visibility";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
@@ -24,10 +32,15 @@ function LoginForm() {
 
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const [credentials, setCredentials] = useState<Credentials>({
     username: "",
     password: "",
   });
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = (e: MouseEvent<HTMLButtonElement>) =>
+    e.preventDefault();
 
   const handleLoginForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -44,77 +57,77 @@ function LoginForm() {
   };
 
   return (
-    <form noValidate onSubmit={handleLoginForm} className={classes.form}>
-      {loading ? (
-        <>
-          <br />
-          <LinearProgress />
-        </>
-      ) : (
-        <>
-          <TextField
-            required
-            fullWidth
-            autoFocus
-            id="username"
-            margin="normal"
-            name="username"
-            label="Username"
-            variant="outlined"
-            error={error.length !== 0}
-            autoComplete="username"
-            value={credentials.username}
-            onChange={(e) =>
-              setCredentials({ ...credentials, username: e.target.value })
-            }
-          />
-          <TextField
-            required
-            fullWidth
-            id="password"
-            margin="normal"
-            name="password"
-            type="password"
-            label="Password"
-            variant="outlined"
-            helperText={error}
-            error={error.length !== 0}
-            autoComplete="password"
-            value={credentials.password}
-            onChange={(e) =>
-              setCredentials({ ...credentials, password: e.target.value })
-            }
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            fullWidth
-            type="submit"
-            color="primary"
-            variant="contained"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link component={RouterLink} to="/register" variant="body2">
-                {"Don't have an account? Sign Up today."}
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-          </Grid>
-          <Box mt={5}>
-            <AuthFormFooter />
-          </Box>
-        </>
-      )}
+    <form onSubmit={handleLoginForm} className={classes.form}>
+      {loading && <LinearProgress />}
+      <TextField
+        required
+        fullWidth
+        autoFocus
+        id="username"
+        margin="normal"
+        name="username"
+        label="Username"
+        variant="outlined"
+        error={error.length !== 0}
+        autoComplete="username"
+        value={credentials.username}
+        onChange={(e) =>
+          setCredentials({ ...credentials, username: e.target.value })
+        }
+      />
+      <FormControl required fullWidth variant="outlined" margin="normal">
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <OutlinedInput
+          id="password"
+          labelWidth={85}
+          error={error.length !== 0}
+          value={credentials.password}
+          type={showPassword ? "text" : "password"}
+          onChange={(e) =>
+            setCredentials({ ...credentials, password: e.target.value })
+          }
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                aria-label="toggle password visibility"
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+        <FormHelperText error={error.length !== 0}>{error}</FormHelperText>
+      </FormControl>
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />
+      <Button
+        fullWidth
+        type="submit"
+        color="primary"
+        variant="contained"
+        className={classes.submit}
+      >
+        Sign In
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link component={RouterLink} to="/register" variant="body2">
+            {"Don't have an account? Sign Up today."}
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </Grid>
+      </Grid>
+      <Box mt={5}>
+        <AuthFormFooter />
+      </Box>
     </form>
   );
 }
